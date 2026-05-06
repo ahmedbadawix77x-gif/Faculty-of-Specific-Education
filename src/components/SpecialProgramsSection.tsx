e65ff08876365b1376f1f9e479eac338aa667817
Ahmed Badawi <ahmedbadawix77x@gmail.com>
2026-05-06 18:28:43 +0300

    UI: Change special programs layout to 3 columns for consistency

--- src/components/SpecialProgramsSection.tsx
+++ src/components/SpecialProgramsSection.tsx
@@ -42,7 +42,7 @@ export default function SpecialProgramsSection() {
 
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {programs.map((program) => (
             <VideoCard
               key={program.id}
